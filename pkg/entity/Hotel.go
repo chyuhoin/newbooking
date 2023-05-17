fc3c3c6f2dc3017c696fa3e8a7a04f248f4fd501
chyuhoin <694546200@qq.com>
2023-05-17 21:05:15 +0800

    旅馆添加绿色值和评论数

--- pkg/entity/Hotel.go
+++ pkg/entity/Hotel.go
@@ -1,12 +1,14 @@
 package entity
 
 type Hotel struct {
-	Id       int     `json:"id"`
-	Name     string  `json:"name"`
-	Province string  `json:"province"`
-	City     string  `json:"city"`
-	Location string  `json:"location"`
-	Rating   float64 `json:"rating"`
-	Star     int8    `json:"star"`
-	Distance float64 `json:"distance"`
+	Id         int     `json:"id"`
+	Name       string  `json:"name"`
+	Province   string  `json:"province"`
+	City       string  `json:"city"`
+	Location   string  `json:"location"`
+	GreenLevel int     `json:"greenLevel"`
+	Comment    int     `json:"comment"`
+	Rating     float64 `json:"rating"`
+	Star       int8    `json:"star"`
+	Distance   float64 `json:"distance"`
 }
