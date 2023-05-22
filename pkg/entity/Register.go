4715b9f610d4a205cbe25b7e88634b16758f2064
chyuhoin <694546200@qq.com>
2023-05-22 19:20:50 +0800

    bugfix: isDeleted莫名消失

--- pkg/entity/Register.go
+++ pkg/entity/Register.go
@@ -1,18 +1,18 @@
 package entity
 
 type Register struct {
-	Id             string `xorm:"pk id" json:"id,omitempty"`
-	RoomId         string `xorm:"room_id" json:"roomId,omitempty"`
-	StartTime      string `xorm:"start_time" json:"startTime,omitempty"`
-	EndTime        string `xorm:"end_time" json:"endTime,omitempty"`
-	UserId         string `xorm:"user_id" json:"userId,omitempty"`
-	BookerName     string `xorm:"b_name" json:"bookerName,omitempty"`
-	BookerEmail    string `xorm:"b_email" json:"bookerEmail,omitempty"`
-	BookerState    string `xorm:"b_state" json:"bookerState,omitempty"`
-	BookerPhone    string `xorm:"b_phone" json:"bookerPhone,omitempty"`
-	BookerBirthday string `xorm:"b_birthday" json:"bookerBirthday,omitempty"`
-	RoomerName     string `xorm:"r_name" json:"roomerName,omitempty"`
-	RoomerEmail    string `xorm:"r_email" json:"roomerEmail,omitempty"`
-	Plan           string `xorm:"plan" json:"plan,omitempty"`
-	IsDeleted      bool   `xorm:"is_deleted" json:"isDeleted,omitempty"`
+	Id             string `xorm:"pk id" json:"id"`
+	RoomId         string `xorm:"room_id" json:"roomId"`
+	StartTime      string `xorm:"start_time" json:"startTime"`
+	EndTime        string `xorm:"end_time" json:"endTime"`
+	UserId         string `xorm:"user_id" json:"userId"`
+	BookerName     string `xorm:"b_name" json:"bookerName"`
+	BookerEmail    string `xorm:"b_email" json:"bookerEmail"`
+	BookerState    string `xorm:"b_state" json:"bookerState"`
+	BookerPhone    string `xorm:"b_phone" json:"bookerPhone"`
+	BookerBirthday string `xorm:"b_birthday" json:"bookerBirthday"`
+	RoomerName     string `xorm:"r_name" json:"roomerName"`
+	RoomerEmail    string `xorm:"r_email" json:"roomerEmail"`
+	Plan           string `xorm:"plan" json:"plan"`
+	IsDeleted      bool   `xorm:"is_deleted" json:"isDeleted"`
 }
